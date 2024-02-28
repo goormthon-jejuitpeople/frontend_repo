ae54912f7d7944d979014a555aca20361c57c985
khv2644511 <khc2644511@naver.com>
2024-02-28 11:08:17 +0900

    ✨ Feat: \"오름 api를 통해 map에 오름 마커 찍기\"

--- src/pages/mainPage/Landing.tsx
+++ src/pages/mainPage/Landing.tsx
@@ -3,7 +3,8 @@ import NearbyStoreMap from '@components/kakaoMap';
 const Landing = () => {
 	return (
 		<div>
-			<NearbyStoreMap searchKeyword={'스타벅스'} />
+			{/* <NearbyStoreMap searchKeyword={'오름'} /> */}
+			<NearbyStoreMap />
 		</div>
 	);
 };

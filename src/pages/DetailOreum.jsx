efe8fdb36de1cfac022e5118bb6e5d1cc7819393
Jo Seung Hyo <seunghyo7742@naver.com>
2024-02-29 00:26:45 +0900

    Detail page 작업중

--- src/pages/DetailOreum.jsx
+++ src/pages/DetailOreum.jsx
@@ -1,5 +1,5 @@
 import { useParams, useSearchParams } from 'react-router-dom';
-
+import logo from '../assets/logo.png';
 const DetailOreum = () => {
 	const { detailId } = useParams();
 	console.log('page _id, detailId');
@@ -7,7 +7,19 @@ const DetailOreum = () => {
 	const queryList = [...searchParams];
 	const oreumData = Object.fromEntries(queryList);
 	console.log('oreumData', oreumData);
-	return <div></div>;
+	return (
+		<main>
+			<header className='bg-[#FFF9F6] flex justify-center pt-6 pb-4'>
+				<img src={logo} />
+			</header>
+			<section>
+				<div className='h-48 overflow-hidden '>
+					<img className='w-full h-full' src={oreumData.imgPath} />
+				</div>
+				<div></div>
+			</section>
+		</main>
+	);
 };
 
 export default DetailOreum;

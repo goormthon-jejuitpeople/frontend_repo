918b98127390a8a82b3a24d73000ccd43593c5ce
khv2644511 <khc2644511@naver.com>
2024-03-13 14:42:11 +0900

    🐛 Fix: cORS ERROR 해결을 위한 프록시 추가

--- src/api/kogpt_api.ts
+++ src/api/kogpt_api.ts
@@ -2,15 +2,15 @@ export async function summarizeReview(reviewText: string, creativity: number) {
 	const requestBody = {
 		prompt: reviewText,
 		max_tokens: 100,
-		temperature: creativity,
-		top_p: 0,
+		// temperature: creativity,
+		// top_p: 0,
 	};
 	try {
 		const response = await fetch('/v1/inference/kogpt/generation', {
 			method: 'POST',
 			headers: {
-				credentials: 'include',
-				Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_REST_API_KEY}`,
+				// Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_REST_API_KEY}`,
+				Authorization: `KakaoAK eea100c490ea4dff5f9edeaf39d31a29`,
 				'Content-Type': 'application/json',
 			},
 			body: JSON.stringify(requestBody),
@@ -23,3 +23,17 @@ export async function summarizeReview(reviewText: string, creativity: number) {
 		console.error('Error calling the KoGPT API:', error);
 	}
 }
+
+// return axios({
+//     method: 'POST',
+//     headers: {
+//       'content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+//     },
+//     url: 'https://kauth.kakao.com/oauth/token',
+//     data: makeFormData({
+//       grant_type: 'authorization_code',
+//       client_id: JS_APP_KEY,
+//       redirect_uri: REDIRECT_URI,
+//       code,
+//     })
+//   })
